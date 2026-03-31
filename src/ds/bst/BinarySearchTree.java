90cb1bf46cf53fcf3fb3d20acf54c99870115657
Anna Yang <nyang27@uis.edu>
2026-03-30 19:23:41 -0500

    Add private Node inner class with left/right children and data Node constructor

--- src/ds/bst/BinarySearchTree.java
+++ src/ds/bst/BinarySearchTree.java
@@ -1,5 +1,15 @@
 package ds.bst;
 
 public class BinarySearchTree <T extends Comparable<? super T>> {
+	
+	private class Node {
+		private Node leftChild, rightChild;
+		private T data;
+		
+		
+		public Node(T data) {
+			this.data = data;
+		}
+	}
 
 }

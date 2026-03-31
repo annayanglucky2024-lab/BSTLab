259927d2058d469cf6be8af6dd6433a3deb6d44d
Anna Yang <nyang27@uis.edu>
2026-03-30 19:30:31 -0500

    add reference, getSize() and isEmpty() methods

--- src/ds/bst/BinarySearchTree.java
+++ src/ds/bst/BinarySearchTree.java
@@ -2,6 +2,16 @@ package ds.bst;
 
 public class BinarySearchTree <T extends Comparable<? super T>> {
 	
+	private Node root;
+	private int size;
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
 	private class Node {
 		private Node leftChild, rightChild;
 		private T data;
